fda8ad85d74a74970350dc48b38995e2f09537ea
Pietro Bucker <pietro_177@hotmail.com>
2022-09-21 20:17:32 -0300

    exemplo switch case

---
+++ Modulo1-Fundamentos/Bloco4-Introduçao-JS/Dia1-introduçao-JS/scripts_swich_case.js
@@ -0,0 +1,15 @@
+let stats ="lista";
+
+switch(stats){
+  case "aprovado":
+    console.log('parabens voce foi aprovado');
+    break;
+  case "reprovado aprovado":
+    console.log('voce foi reprovado');
+    break;  
+  case "lista":
+    console.log('voce esta na nosa lista de espera');
+    break;  
+  default:
+    console.log("informaçao incorreta");  
+}
