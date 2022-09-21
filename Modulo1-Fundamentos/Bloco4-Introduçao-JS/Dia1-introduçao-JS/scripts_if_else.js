59d2172226dbfc7538e63d3799ddb4ca81b43c55
Pietro Bucker <pietro_177@hotmail.com>
2022-09-21 18:51:59 -0300

    exemplos if else

---
+++ Modulo1-Fundamentos/Bloco4-Introduçao-JS/Dia1-introduçao-JS/scripts_if_else.js
@@ -0,0 +1,11 @@
+const nota = 65;
+
+if(nota >= 80){
+  console.log("Voce esta Aprovado");
+}
+else if(nota < 80 && nota >= 60){
+  console.log("Voce esta na lista de Espera");
+}
+else{
+  console.log("Voce foi Reprovado");
+}
