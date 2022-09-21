afbf01fb78de1c40f2a6c3ff089fee04aaa917f5
Pietro Bucker <pietro_177@hotmail.com>
2022-09-21 20:37:14 -0300

    exercio parte 1 e 2. ok

---
+++ Modulo1-Fundamentos/Bloco4-Introduçao-JS/Dia1-introduçao-JS/scripts_exercicio.js
@@ -0,0 +1,23 @@
+// -----------parte 1------------
+const a = 10;
+const b = 5;
+let adicao = a + b;
+let subtr = a - b;
+let multp = a * b;
+let divi = a / b;
+let mod = a % b;
+console.log(adicao, subtr, multp, divi, mod);
+
+// ---------part 2 --------------
+
+const c = 15;
+
+if(a > b && a > c){
+  console.log(a + " é o maior numero");
+}
+else if(b > a && b > c){
+  console.log(b + " é o maior numero");
+}
+else{
+  console.log(c + " é o maior numereo");
+}
