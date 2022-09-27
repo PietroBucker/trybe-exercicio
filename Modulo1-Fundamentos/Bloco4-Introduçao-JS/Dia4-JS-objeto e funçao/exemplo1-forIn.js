9d7b607220580228915a7a3e4882914824cfe37d
Pietro Bucker <pietro_177@hotmail.com>
2022-09-27 00:07:38 -0300

    exemplo e exercicios feitos

---
+++ Modulo1-Fundamentos/Bloco4-Introduçao-JS/Dia4-JS-objeto e funçao/exemplo1-forIn.js
@@ -0,0 +1,10 @@
+let names = {
+  person1: 'João',
+  person2: 'Maria',
+  person3: 'Jorge'
+};
+for(let name in names){
+  console.log(name + ' = ' + names[name]);
+}
+//-------------------------------
+
