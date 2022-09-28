e7a31cc0c623614889479848b5ec428e98d74547
Pietro Bucker <pietro_177@hotmail.com>
2022-09-28 16:02:22 -0300

    exercicio mod 5 dia 1

---
+++ Modulo1-Fundamentos/Bloco5-JS-DOM-EVENTS-WEBstorage/Dia1-DOM-SELETORES/script.js
@@ -0,0 +1,5 @@
+const paragraph = document.getElementsByClassName("paragraph");
+paragraph[0].style.color = "red";
+paragraph[1].style.color = "green";
+const paragraph2 = document.getElementsByTagName("h2");
+paragraph2[0].style.background = "blue";
