b535d7b02554491b221905cf46346d8faa1221b2
Pietro Bucker <pietro_177@hotmail.com>
2022-09-29 23:03:42 -0300

    exercicio 5.2 finalizado

--- Modulo1-Fundamentos/Bloco5-JS-DOM-EVENTS-WEBstorage/Dia2-JS trabalhando elementos/scripts.js
+++ Modulo1-Fundamentos/Bloco5-JS-DOM-EVENTS-WEBstorage/Dia2-JS trabalhando elementos/scripts.js
@@ -6,12 +6,33 @@ function creatTag (tags, classe, parents){
   // console.log(tag);
   parent.appendChild(tag);
 }
-creatTag('h1', 'titulo', 'body');
+creatTag('h1', 'title', 'body');
 creatTag('main', 'main-content', 'body');
 creatTag('section', 'center-content', '.main-content');
 creatTag('p', 'paragraf1-cent-cont', '.center-content');
 creatTag('section', 'left-content', '.main-content');
 creatTag('section', 'right-content', '.main-content');
+creatTag('img', 'small-image', '.left-content');
+creatTag('ul', 'u-lista-numeros', '.right-content');
+creatTag('h3', 'description', '.main-content');
+creatTag('h3', 'description', '.main-content');
+creatTag('h3', 'description', '.main-content');
+
+
+
+
+//criando a <ul>
+function criaUl (parents, listItens){
+  const parent = document.querySelector(parents);
+  for(let i = 0; i < listItens.length; i+= 1){
+    let valorLi = listItens[i];
+
+    let criaLi = document.createElement('li');
+    criaLi.innerText = valorLi;
+    parent.appendChild(criaLi);
+  }
+}
+criaUl('.u-lista-numeros', ['um', 'dois', 'tres', 'quatro', 'cinco', 'seis', 'sete', 'oito', 9, 'dez']);
 
 //ADD textos
 function addText (parents, text){
@@ -23,13 +44,27 @@ addText('.paragraf1-cent-cont', 'Testando o DOM');
 
 //ADD imagem
 // const acess = document.querySelector('.left-content');
-const acess = document.querySelector('.main-content');
-const imagen = document.createElement('img');
-imagen.className = 'small-image';
-imagen.src = 'https://picsum.photos/200';
-acess.children[1].appendChild(imagen);
+// dpois acessar o smal-imag pelo firstChild
+document.querySelector('.small-image').src = 'https://picsum.photos/200';;
 
-const lista = document.createElement('ul');
-lista.className = 'u-lista-numeros';
+//remover tags
+function removeTag(parents, tags){
+let parent = document.querySelector(parents);
+let tag = document.querySelector(tags);
+parent.removeChild(tag);
+}
+// removeTag('.main-content', '.left-content');
+let remove = document.querySelector('.u-lista-numeros');
+let removLi = document.querySelectorAll('.u-lista-numeros li');
+for(let iR = 0; iR < removLi.length; iR += 1){
+  let compara =removLi[iR];
+  if(compara.innerText.includes('9') || compara.innerText.includes('dez')){
+    remove.removeChild(compara);
+  }
+  
+}
 
-acess.children[2].appendChild(lista);
+
+const editElement = document.querySelector('main');
+editElement.children[2].style.marginRight = 'auto';
+editElement.children[0].style.background = 'green';
