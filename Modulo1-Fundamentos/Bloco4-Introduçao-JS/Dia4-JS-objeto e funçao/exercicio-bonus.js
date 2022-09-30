532319783292b0853972a4e7b4cba2237ca48d37
Pietro Bucker <pietro_177@hotmail.com>
2022-09-30 14:14:12 -0300

    Exercicio bonus 1 feito

--- Modulo1-Fundamentos/Bloco4-Introduçao-JS/Dia4-JS-objeto e funçao/exercicio-bonus.js
+++ Modulo1-Fundamentos/Bloco4-Introduçao-JS/Dia4-JS-objeto e funçao/exercicio-bonus.js
@@ -6,13 +6,47 @@ function calcRomanos(numeros){
     IX: 9,
     X: 10,
     XL:40,
-    L: 50
+    L: 50,
+    XC: 90,
+    C: 100,
+    CD: 400,
+    D: 500,
+    CM:900,
+    M:1000
   }
   console.log('test');
-  const ordenaRomano = numeros.split('');
-  console.log(ordenaRomano);
-  for(let ind in romano){
-    console.log(romano[ind]);
+  const ordenaRomano = numeros.toUpperCase(numeros.split(''));
+  console.log(romano['I']);
+  let convertNumber = 0;
+  let getNumber= [];
+  for(let i = 0 ; i < ordenaRomano.length; i +=1){
+    for(let jI in romano){
+      console.log(jI);
+      if(ordenaRomano[i] === jI){
+       convertNumber = romano[jI];
+        getNumber.push(convertNumber);
+      }
+    }
+
   }
+  let somaNumber = 0;
+  let recebeSoma = 0;
+  console.log(getNumber);    
+  for(let i = 0; i < getNumber.length; i += 1){
+       if(getNumber[i] < getNumber[i + 1]){
+      somaNumber = Math.abs(getNumber[i] - getNumber[i+1]);
+      getNumber.splice(0,2,0)
+      console.log(getNumber);
+      recebeSoma += somaNumber;
+    }
+    else{
+      recebeSoma += getNumber[i];
+      
+    }
+  } 
+   
+   return recebeSoma; 
+    
+   
 }
-console.log(calcRomanos('XLIV'));
+console.log(calcRomanos('xxx'));
