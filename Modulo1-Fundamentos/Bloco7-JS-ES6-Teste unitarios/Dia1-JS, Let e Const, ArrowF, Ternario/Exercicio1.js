94a8573ffaef43562e2794ef67c148c026676e34
Pietro Bucker <pietro_177@hotmail.com>
2022-10-17 18:37:17 -0300

    exercicio 1 feito

---
+++ Modulo1-Fundamentos/Bloco7-JS-ES6-Teste unitarios/Dia1-JS, Let e Const, ArrowF, Ternario/Exercicio1.js
@@ -0,0 +1,19 @@
+// const testingScope = (escopo) => {
+//   if (escopo === true) {
+//     let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+//     ifScope = `${ifScope} ótimo, fui utilizada no escopo !`;
+//     console.log(ifScope);
+//   } else {
+//     let elseScope = 'Não devo ser utilizada fora do meu escopo (else)';
+//     console.log(elseScope);
+//   }
+// }
+
+// testingScope(false);
+
+// -------- parte 2 ----------
+
+const oddsAndEvens = [13, 3, 4, 10, 7, 2, 2];
+
+const sortOddsAndEvens = (param) => param.sort(funct = (a, b) => (a - b));
+console.log(sortOddsAndEvens(oddsAndEvens));
