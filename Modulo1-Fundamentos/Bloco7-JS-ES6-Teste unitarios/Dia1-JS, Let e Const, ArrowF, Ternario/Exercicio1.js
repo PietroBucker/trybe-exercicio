9c1a922b17e9d81280f45e7e8a2efb428470b996
Pietro Bucker <pietro_177@hotmail.com>
2022-10-17 20:51:56 -0300

    2 parte 1 feito

--- Modulo1-Fundamentos/Bloco7-JS-ES6-Teste unitarios/Dia1-JS, Let e Const, ArrowF, Ternario/Exercicio1.js
+++ Modulo1-Fundamentos/Bloco7-JS-ES6-Teste unitarios/Dia1-JS, Let e Const, ArrowF, Ternario/Exercicio1.js
@@ -13,7 +13,27 @@
 
 // -------- parte 2 ----------
 
-const oddsAndEvens = [13, 3, 4, 10, 7, 2, 2];
+// const oddsAndEvens = [13, 3, 4, 10, 7, 2, 2];
 
-const sortOddsAndEvens = (param) => param.sort(funct = (a, b) => (a - b));
-console.log(sortOddsAndEvens(oddsAndEvens));
+// const sortOddsAndEvens = (param) => param.sort(funct = (a, b) => (a - b));
+// console.log(sortOddsAndEvens(oddsAndEvens));
+
+// ..............................................sem usar sort...................................
+
+// let oddsAndEvens2 = [13, 3, 4, 10, 7, 2];
+
+// const sortOddsAndEvens2 = (param) => {
+// let aux = 0; 
+//   for(let i = 0; i < param.length; i += 1){
+//     for(let j = 0; j < param.length; j += 1){ 
+//       if(param[i] < param[j]){
+//         aux = param[j];
+//         param[j] = param[i];
+//         param[i] = aux;
+//       }
+//     }
+//   }
+
+//   return param;
+// }
+// console.log(sortOddsAndEvens2(oddsAndEvens2));
